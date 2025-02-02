05d4b15f58097561dda9bc85adaadd85c311138c
Dokey <asgerjd@gmail.com>
2025-02-02 23:04:50 +0100

    h

---
+++ js/list.js
@@ -0,0 +1,32 @@
+const productliste = document.querySelector("#productlist");
+let url = `https://kea-alt-del.dk/t7/api/products`;
+
+function showProducts(data) {
+  const markup = data
+    .map(
+      (product) => `
+      <a href="produkt.html?id=${product.id}">
+  <article class="product_card">
+    <img src="${`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`}">
+    <div>
+      <h3>${product.productdisplayname}</h3>
+      <p>kr. <span>${product.price}</span>,-</p>
+      ${
+        product.discount ? `<p>rabat <span>${product.discount}</span>%</p>` : ""
+      }
+    </div>
+  </article>
+</a>`
+    )
+    .join("");
+
+  productliste.innerHTML = markup;
+}
+
+function hentData() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showProducts(data));
+}
+
+hentData();
