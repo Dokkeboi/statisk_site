a898db68982c9ff866cee45b6ff0cad5abf3c904
Dokey <asgerjd@gmail.com>
2025-02-06 17:21:56 +0100

    added filters

--- js/list.js
+++ js/list.js
@@ -4,10 +4,30 @@ const myCategory = urlParams.get("category");
 
 const productliste = document.querySelector("main");
 let url = `https://kea-alt-del.dk/t7/api/products?limit=100`;
-
 if (myCategory) {
-  url = `https://kea-alt-del.dk/t7/api/products?category=${myCategory}`;
+  url = `https://kea-alt-del.dk/t7/api/products?category=${myCategory}&limit=100`;
 }
+
+document.querySelectorAll("button").forEach((knap) => knap.addEventListener("click", showFiltered));
+
+function showFiltered() {
+  const filter = this.dataset.gender;
+  if (filter == "All") {
+    showProducts(allData);
+  } else {
+    fraction = allData.filter((product) => product.gender === filter);
+    showProducts(fraction);
+  }
+}
+
+let allData;
+
+fetch(url)
+  .then((response) => response.json())
+  .then((json) => {
+    allData = json;
+    showProducts(allData);
+  });
 
 function showProducts(data) {
   const markup = data
@@ -22,11 +42,7 @@ function showProducts(data) {
         <h3>${product.productdisplayname}</h3>
         <p class="subtle">${product.articletype} | ${product.brandname}</p>
         <p class="price">DKK ${product.price},-</p>
-        ${
-          product.discount
-            ? `<p class="discount">Rabat <span>${product.discount}</span>%</p>`
-            : ""
-        }
+        ${product.discount ? `<p class="discount">Rabat <span>${product.discount}</span>%</p>` : ""}
         <a href="produkt.html?id=${product.id}">Read More</a>
       </article>`
     )
